4d67d3c59bcd0991cb2de9cf6bf272916c6faa81
Luluuu66 <lu7225547@gmail.com>
2026-03-10 01:11:31 +0800

    fix: 解決衝突並確認刪除 test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-with open("backEnd/strategy.py", "r", encoding="utf-8") as f:
-    fileContent = f.read()
-front = fileContent.split("# insertCode")[0]
-back = fileContent.split("# insertCode")[1]
-with open("strategy/test.py", "r", encoding="utf-8") as f:
-    fileContent = f.read()
-all = front + fileContent + back
-print(all)
